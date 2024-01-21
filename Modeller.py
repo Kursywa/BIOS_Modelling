5d140a203d656fbc1326a17e34a85068366c1477
Kursywa <masterortografii@gmail.com>
2024-01-21 17:38:25 +0100

    reworking

--- Modeller.py
+++ Modeller.py
@@ -3,13 +3,13 @@ from modeller.automodel import *
 
 def main():
     env = Environ()
-    a = AutoModel(env, alnfile = './Preprocessing_Data/MSA_Alignment.txt',
-    knowns = ('3J4Q_D', '6F14_A', '6Y05_A', '1CTP_E'), sequence = 'cAMP_kinase')
+    a = AutoModel(env, alnfile = './Preprocessing_Data/MSA_Alignment.pir',
+    knowns = ('3j4q', '6f14', '1CTP'), sequence = 'UN_SEQ')
     a.starting_model = 1
     a.ending_model = 5
     a.make()
     a.write('Final_Model', format ='PDB')
 
-
+# 6yz5, 1CTP,
 if __name__ == '__main__':
     main()
